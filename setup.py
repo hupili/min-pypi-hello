3c5a93076c92f204a5cb535f380a8063c5a58f7d
HU Pili <hpl1989@gmail.com>
2013-07-11 22:28:58 +0800

    fix file include

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='minpypihello',
-      version='1.3',
+      version='1.4',
       description='Minimal PyPI Package Example',
       author='Pili Hu',
       author_email='me@hupili.net',
